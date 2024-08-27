54e51d75081d390dafd50e5b91cea0bb956581fc
Nguyen Lam <nguyen@cngsoftware.com>
2024-08-26 22:05:14 -0500

    Fix buildpack and build script issues for Heroku

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Serve static files from the React app's build directory
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 // Handle all GET requests by returning the React app's HTML file
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
 app.listen(port, () => {
